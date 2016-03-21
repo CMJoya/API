9409e449c6227a9b776c5d7286e8c57b046ea9f0
Cris Joya <MJoya@joya.attlocal.net>
2016-03-20 22:22:38 -0500

    Made Changes In Model User, Partial Jbuilder, And In Routes

--- app/views/api/posts/_post.json.jbuilder
+++ app/views/api/posts/_post.json.jbuilder
@@ -1,4 +1,2 @@
 json.id post.id
 json.title post.title
-json.post_as_int post.post_date.to_i
-json.formatted_date distance_of_time_in_words(post.post_date, Time.now)
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     get "posts" => 'posts#index'
     get "posts/:id" => 'posts#show'
     post "posts" => 'posts#create'
-    patch "posts/:id" => 'posts#update'
+    put "posts/:id" => 'posts#update'
     delete "posts/:id" => 'posts#delete'
 
     post "users" => 'registrations#create'
